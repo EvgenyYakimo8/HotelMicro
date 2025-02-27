e678991b842880668034788dcb6c53726c4865d0
EvgenyYakimo8 <colos0117@gmail.com>
2025-02-27 10:09:29 +0300

    nitial commit

---
+++ src/main/java/by/com/gpsolutions/hotelmicro/HotelMicroApplication.java
@@ -0,0 +1,13 @@
+package by.com.gpsolutions.hotelmicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelMicroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelMicroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/com/gpsolutions/hotelmicro/HotelMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package by.com.gpsolutions.hotelmicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelMicroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
